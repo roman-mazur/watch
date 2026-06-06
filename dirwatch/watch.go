e1cc84ef77a035adbc1edb9834909adf895004c9
Roman Mazur <roman@rmazur.io>
2026-06-06 21:04:32 +0200

    dirwatch: extract reusable package

---
+++ dirwatch/watch.go
@@ -0,0 +1,73 @@
+package dirwatch
+
+import (
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func Watch(p string, signals chan string) error {
+	w, err := fsnotify.NewWatcher()
+	if err != nil {
+		return err
+	}
+	defer w.Close()
+
+	for _, wp := range collectWatchPaths(p) {
+		if err := w.Add(wp); err != nil {
+			return err
+		}
+	}
+
+	processEvents(w, signals)
+	return nil
+}
+
+func collectWatchPaths(dir string) []string {
+	res := []string{dir}
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return res
+	}
+	for _, entry := range entries {
+		if entry.IsDir() {
+			res = append(res, collectWatchPaths(filepath.Join(dir, entry.Name()))...)
+		}
+	}
+	return res
+}
+
+func processEvents(w *fsnotify.Watcher, out chan string) {
+	defer close(out)
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	var changedPaths []string
+
+	for {
+		select {
+		case e := <-w.Events:
+			if e.Has(fsnotify.Write) || e.Has(fsnotify.Rename) || e.Has(fsnotify.Create) || e.Has(fsnotify.Remove) {
+				changedPaths = append(changedPaths, e.Name)
+			}
+			if e.Has(fsnotify.Create) {
+				fi, err := os.Stat(e.Name)
+				if err == nil && fi.IsDir() {
+					_ = w.Add(e.Name)
+				}
+			}
+		case <-w.Errors:
+			return
+
+		case <-ticker.C:
+			if len(changedPaths) > 0 {
+				for _, p := range changedPaths {
+					out <- p
+				}
+				changedPaths = changedPaths[:0]
+			}
+		}
+	}
+}
--- main.go
+++ main.go
@@ -2,7 +2,8 @@
 // the provided command when there is a write-like event.
 // Writes to the child directories including newly created are also handled.
 // Usage:
-// 		watch . go test ./...
+//
+//	watch . go test ./...
 package main
 
 import (
@@ -10,10 +11,8 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"path/filepath"
-	"time"
 
-	"github.com/fsnotify/fsnotify"
+	"rmazur.io/watch/dirwatch"
 )
 
 var verbose = flag.Bool("v", false, "verbose mode")
@@ -26,83 +25,24 @@ func main() {
 	}
 	logf("watching %s", watchPath)
 
-	w, err := fsnotify.NewWatcher()
-	if err != nil {
-		panic(err)
-	}
-	defer w.Close()
-
-	for _, wp := range collectWatchPaths(watchPath) {
-		if err := w.Add(wp); err != nil {
-			panic(err)
-		}
-	}
-
-	signals := make(chan struct{})
+	signals := make(chan string)
 	go func() {
-		args := flag.Args()[1:]
-		logf("cmd: %s", args)
-		for range signals {
-			execute(args)
+		err := dirwatch.Watch(watchPath, signals)
+		if err != nil {
+			panic(err)
 		}
 	}()
 
-	processEvents(w, signals)
+	args := flag.Args()[1:]
+	logf("cmd: %s", args)
+	for range signals {
+		execute(args)
+	}
 }
 
 func logf(fmt string, args ...any) {
 	if *verbose {
 		log.Printf(fmt, args...)
-	}
-}
-
-func collectWatchPaths(dir string) []string {
-	res := []string{dir}
-	entries, err := os.ReadDir(dir)
-	if err != nil {
-		return res
-	}
-	for _, entry := range entries {
-		if entry.IsDir() {
-			logf("expanding to %s", entry.Name())
-		}
-		res = append(res, collectWatchPaths(filepath.Join(dir, entry.Name()))...)
-	}
-	return res
-}
-
-func processEvents(w *fsnotify.Watcher, out chan struct{}) {
-	defer close(out)
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
-
-	trigered := false
-
-	for {
-		select {
-		case e := <-w.Events:
-			if *verbose {
-				log.Println("event:", e)
-			}
-			if e.Has(fsnotify.Write) || e.Has(fsnotify.Rename) || e.Has(fsnotify.Create) {
-				trigered = true
-			}
-			if e.Has(fsnotify.Create) {
-				fi, err := os.Stat(e.Name)
-				if err == nil && fi.IsDir() {
-					w.Add(e.Name)
-				}
-			}
-		case err := <-w.Errors:
-			log.Println(err)
-			return
-
-		case <-ticker.C:
-			if trigered {
-				trigered = false
-				out <- struct{}{}
-			}
-		}
 	}
 }
 
